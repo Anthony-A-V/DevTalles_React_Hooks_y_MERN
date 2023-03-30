f8fc5b268ac44e4a2dc7360b689b8bde0248c221
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-03-29 22:38:45 -0500

    first commit

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,19 @@
+
+const apiKey = '78Q6G6ntd01m1jjT2V3u3RtI1fBqH5FW';
+
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+
+peticion
+    .then( resp => resp.json() )
+    .then( ({ data }) => {
+        const { url } = data.images.original;
+        
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append( img );
+
+
+    })
+    .catch( console.warn );
+
