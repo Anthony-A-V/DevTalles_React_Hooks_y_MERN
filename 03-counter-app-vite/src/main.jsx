3271acc1078df4efa99fab27f131cbe29e130c5f
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-04-01 16:45:15 -0500

    cambios

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <HelloWorldApp /> */}
-    <FirstApp title="Hola, soy Goku" subTitle={123} />
+    {/* <FirstApp title="Hola, soy Goku" subTitle={123} /> */}
+    <CounterApp value={1} />
   </React.StrictMode>
 );
