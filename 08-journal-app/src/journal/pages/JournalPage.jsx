abea916bc828cd64f0c434ce90410c50dc8c90ee
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-10-20 12:07:29 -0500

    Sección 17

---
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -0,0 +1,36 @@
+import { IconButton } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      {/* <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur minima
+        nemo exercitationem qui nulla, quaerat ex est quis ipsa voluptas tempora
+        illum? Sed, cum in nihil ipsam eligendi minima eveniet!
+      </Typography> */}
+
+      {/* <NothingSelectedView /> */}
+      <NoteView />
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {
+            backgroundColor: 'error.main',
+            opacity: 0.9,
+          },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}></AddOutlined>
+      </IconButton>
+    </JournalLayout>
+  );
+};
