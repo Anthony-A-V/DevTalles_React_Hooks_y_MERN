cb732cfb8ca40014672c3dcc2220f5e7a9512259
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-10-20 12:07:29 -0500

    Sección 18

---
+++ 08-journal-app/src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { AuthRoutes } from '../auth/routes/AuthRoutes';
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      {/* Login y Registro */}
+      <Route path="/auth/*" element={<AuthRoutes />} />
+
+      {/* JournalApp */}
+      <Route path="/*" element={<JournalRoutes />} />
+    </Routes>
+  );
+};
