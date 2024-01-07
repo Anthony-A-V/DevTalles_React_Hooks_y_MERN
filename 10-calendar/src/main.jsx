40bc2fbb5b11a82b75ad880e869739ffde53d47d
Anthony-A-V <anthony.anguizvargas@gmail.com>
2024-01-06 20:00:59 -0500

    Sección 22

---
+++ 10-calendar/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { CalendarApp } from './CalendarApp.jsx';
+import './styles.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <CalendarApp />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
